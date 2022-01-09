a3f750d0982c0380031b0b5dafbf552dc2a664ee
xaxage <xaxage@gmail.com>
2022-01-09 20:42:47 +0400

    Create Joke controller, so it'll invoke the method when the request arrives. In that method we return name of the view that will be shown to the user. Closes #4

---
+++ src/main/java/com/xaxage/springjokesapp/controller/JokeController.java
@@ -0,0 +1,25 @@
+package com.xaxage.springjokesapp.controller;
+
+import com.xaxage.springjokesapp.services.JokeService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class JokeController {
+
+    private final JokeService jokeService;
+
+    @Autowired
+    public JokeController(JokeService jokeService) {
+        this.jokeService = jokeService;
+    }
+
+    @RequestMapping({"/", ""})
+    public String showJoke(Model model) {
+        model.addAttribute("joke", jokeService.getJoke());
+
+        return "index";//This will tell Spring that we want to return view named "index"
+    }
+}
